1f32b15234b3d60b35644fbc8152a54b27d247be
Hamza Amin <hamzabinamin123@gmail.com>
2022-11-30 19:41:08 +0500

    Initial Commit

---
+++ Staff Loads/Staff_LoadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Staff_LoadsApp.swift
+//  Staff Loads
+//
+//  Created by Hamza Amin on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Staff_LoadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
